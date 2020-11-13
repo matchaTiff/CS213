952bc7b9887c15d9d3a2a77b0f7254a4c7534327
Seth Santos <sethsantos789@gmail.com>
2020-11-12 22:25:00 -0500

    Set

---
+++ Project_4/src/Controller.java
@@ -0,0 +1,43 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
+
+import java.awt.*;
+import java.io.File;
+
+public class Controller {
+
+    @FXML private ComboBox sandwichChoices;
+    @FXML private Label ingredients;
+    @FXML private ImageView sandwichImage;
+
+    private Image image;
+
+    public void initialize() {
+        sandwichChoices.getItems().addAll(
+                "Chicken Sandwich",
+                "Beef Sandwich",
+                "Fish Sandwich"
+                );
+        sandwichChoices.getSelectionModel().selectFirst();
+        if( sandwichChoices.getValue().equals("Chicken Sandwich") ) {
+            ingredients.setText("Lettuce, tomato, fried chicken, mayo");
+        }
+    }
+
+    public void comboBox() {
+        if( sandwichChoices.getValue().equals("Chicken Sandwich") ) {
+            sandwichImage.setImage(new Image("chicken_sandwich.png"));
+            ingredients.setText("Lettuce, tomato, fried chicken, mayo");
+        }else if( sandwichChoices.getValue().equals("Beef Sandwich") ){
+            sandwichImage.setImage(new Image("beef_sandwich.png"));
+            ingredients.setText("BEEF INGREDIENTS");
+        }else{
+            sandwichImage.setImage(new Image("fish_sandwich.png"));
+            ingredients.setText("FISH INGREDIENTS");
+        }
+    }
+
+}
