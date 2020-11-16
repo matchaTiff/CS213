ee4ea0108e55bab2c8921ac515225ac2bf82f299
Tiffany <tiffany1259@gmail.com>
2020-11-16 18:58:20 -0500

    add comment

--- Project_4/src/Customizable.java
+++ Project_4/src/Customizable.java
@@ -1,3 +1,10 @@
+/**
+ * Interface class that uses the following methods to add or remove an object to an ArrayList. 
+ * The user of this interface has precise control over what element is to be inserted.
+ * 
+ * @author Seth Santos, Tiffany Chen
+ * 
+ */
 public interface Customizable {
     boolean add(Object obj);
     boolean remove(Object obj);
