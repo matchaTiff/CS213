52dbecd4af9233f237901df0ef8e8837e5531c63
Seth Santos <sethsantos789@gmail.com>
2020-12-07 00:26:12 -0500

    Adds functionality

--- app/src/main/java/com/example/museum/MainActivity.java
+++ app/src/main/java/com/example/museum/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +17,25 @@ public class MainActivity extends AppCompatActivity {
 
     public void museumClicked(View v) {
         // what activity intends to do
+        Button b = (Button)v;
+        String buttonText = b.getText().toString();
+        int buttonPressed =0;
+
+        if (buttonText.equalsIgnoreCase("THE MUSEUM OF MODERN ART")){
+            buttonPressed = 1;
+        }else if(buttonText.equalsIgnoreCase("METROPOLITAN MUSEUM OF ART")){
+            buttonPressed = 2;
+        }else if(buttonText.equalsIgnoreCase("WHITNEY MUSEUM OF AMERICAN ART")){
+            buttonPressed = 3;
+        }else if(buttonText.equalsIgnoreCase("AMERICAN MUSEUM OF NATURAL HISTORY")){
+            buttonPressed = 4;
+        }else{
+            buttonPressed = 0;
+        }
+
+        //System.out.println(buttonText);
         Intent i = new Intent(this,TicketActivity.class);
+        i.putExtra("PressedButton", Integer.toString(buttonPressed));
         startActivity(i);
     }
 }
--- app/src/main/java/com/example/museum/TicketActivity.java
+++ app/src/main/java/com/example/museum/TicketActivity.java
@@ -1,15 +1,41 @@
 package com.example.museum;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class TicketActivity extends AppCompatActivity {
+
+    String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ticket);
+
+        Intent i = getIntent();
+        String buttonNum = i.getStringExtra("PressedButton");
+        ImageView museumImage = (ImageView) findViewById(R.id.museumImage);
+
+        if( buttonNum.equals("1") ){
+            museumImage.setImageResource(R.drawable.museumofmodernart);
+            url = "https://www.moma.org/";
+        }else if( buttonNum.equals("2") ){
+            museumImage.setImageResource(R.drawable.metropolitan);
+            url = "https://www.metmuseum.org/";
+        }else if( buttonNum.equals("3") ){
+            museumImage.setImageResource(R.drawable.whitney);
+            url = "https://whitney.org/";
+        }else if( buttonNum.equals("4") ){
+            museumImage.setImageResource(R.drawable.americanmuseum);
+            url = "https://www.amnh.org/";
+        }else{}
 
         // create a back button
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -17,4 +43,32 @@ public class TicketActivity extends AppCompatActivity {
         // toast
         Toast.makeText(getApplicationContext(), "Maximum of 5 tickets for each!" , Toast.LENGTH_LONG).show();
     }
+
+    public void onClickMuseumImageOne(View v) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        startActivity(intent);
+
+    }
+
+    public void onClickMuseumImageTwo(View v) {
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        startActivity(intent);
+
+    }
+
+    public void onClickMuseumImageThree(View v) {
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        startActivity(intent);
+
+    }
+
+    public void onClickMuseumImageFour(View v) {
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        startActivity(intent);
+
+    }
+
 }
