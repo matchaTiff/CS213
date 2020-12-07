bee3fc4c9759c2a166fc2fac02d63272d566a230
Seth Santos <sethsantos789@gmail.com>
2020-12-07 03:31:38 -0500

    Completes backend of all the buttons to calculate prices

--- app/src/main/java/com/example/museum/TicketActivity.java
+++ app/src/main/java/com/example/museum/TicketActivity.java
@@ -8,12 +8,41 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class TicketActivity extends AppCompatActivity {
 
     String url;
+
+    Spinner adultTix;
+    Spinner seniorTix;
+    Spinner studentTix;
+
+    TextView ticketPriceField;
+    TextView salesTaxField;
+    TextView ticketTotalField;
+
+    TextView adultPrice;
+    TextView seniorPrice;
+    TextView studentPrice;
+
+    double adultTicketPrice;
+    double seniorTicketPrice;
+    double studentTicketPrice;
+
+    final double NY_SALES_TAX = 8.875;
+    final double NJ_SALES_TAX = 6.625;
+    double salesTax;
+
+    // Initializes the ticket prices in the order of Adult, Senior, and Student
+    final double[] MUSEUM_OF_MODERN_ART_TICKETS = new double[] {25.00, 18.00, 14.00};
+    final double[] METROPOLITAN_MUSEUM_OF_ART_TICKETS = new double[] {25.00, 17.00, 12.00};
+    final double[] WHITNEY_MUSEUM_OF_AMERICAN_ART_TICKETS = new double[] {25.00, 18.00, 18.00};
+    final double[] AMERICAN_MUSEUM_OF_NATURAL_HISTORY_TICKETS = new double[] {23.00, 18.00, 18.00};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,24 +54,59 @@ public class TicketActivity extends AppCompatActivity {
         ImageView museumImage = (ImageView) findViewById(R.id.museumImage);
         TextView museumName = (TextView) findViewById(R.id.museumName);
 
+        adultTix = (Spinner) findViewById(R.id.spinner);
+        seniorTix = (Spinner) findViewById(R.id.spinner2);
+        studentTix = (Spinner) findViewById(R.id.spinner3);
+
+        ticketPriceField = (TextView) findViewById(R.id.ticketPriceField);
+        salesTaxField = (TextView) findViewById(R.id.salesTaxField);
+        ticketTotalField = (TextView) findViewById(R.id.ticketTotalField);
+
+        adultPrice = (TextView) findViewById(R.id.adultPrice);
+        seniorPrice = (TextView) findViewById(R.id.seniorPrice);
+        studentPrice = (TextView) findViewById(R.id.studentPrice);
+
+        // Determines the url and image to be displayed
+        // based on the museum choice that is pressed
         if( buttonNum.equals("1") ){
             museumImage.setImageResource(R.drawable.museumofmodernart);
             url = "https://www.moma.org/";
             museumName.setText(R.string.museum1);
-
+            adultTicketPrice = MUSEUM_OF_MODERN_ART_TICKETS[0];
+            seniorTicketPrice = MUSEUM_OF_MODERN_ART_TICKETS[1];
+            studentTicketPrice = MUSEUM_OF_MODERN_ART_TICKETS[2];
+            salesTax = NY_SALES_TAX;
         }else if( buttonNum.equals("2") ){
             museumImage.setImageResource(R.drawable.metropolitan);
             url = "https://www.metmuseum.org/";
             museumName.setText(R.string.museum2);
+            adultTicketPrice = METROPOLITAN_MUSEUM_OF_ART_TICKETS[0];
+            seniorTicketPrice = METROPOLITAN_MUSEUM_OF_ART_TICKETS[1];
+            studentTicketPrice = METROPOLITAN_MUSEUM_OF_ART_TICKETS[2];
+            salesTax = NY_SALES_TAX;
         }else if( buttonNum.equals("3") ){
             museumImage.setImageResource(R.drawable.whitney);
             url = "https://whitney.org/";
             museumName.setText(R.string.museum3);
+            adultTicketPrice = WHITNEY_MUSEUM_OF_AMERICAN_ART_TICKETS[0];
+            seniorTicketPrice = WHITNEY_MUSEUM_OF_AMERICAN_ART_TICKETS[1];
+            studentTicketPrice = WHITNEY_MUSEUM_OF_AMERICAN_ART_TICKETS[2];
+            salesTax = NY_SALES_TAX;
         }else if( buttonNum.equals("4") ){
             museumImage.setImageResource(R.drawable.americanmuseum);
             url = "https://www.amnh.org/";
             museumName.setText(R.string.museum4);
+            adultTicketPrice = AMERICAN_MUSEUM_OF_NATURAL_HISTORY_TICKETS[0];
+            seniorTicketPrice = AMERICAN_MUSEUM_OF_NATURAL_HISTORY_TICKETS[1];
+            studentTicketPrice = AMERICAN_MUSEUM_OF_NATURAL_HISTORY_TICKETS[2];
+            salesTax = NY_SALES_TAX;
         }else{}
+
+
+        // Sets the values for the price fields
+        adultPrice.setText("$" + Double.toString(adultTicketPrice) + "0");
+        seniorPrice.setText("$" + Double.toString(seniorTicketPrice) + "0");
+        studentPrice.setText("$" + Double.toString(studentTicketPrice) + "0");
 
         // create a back button
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -54,8 +118,35 @@ public class TicketActivity extends AppCompatActivity {
     public void onClickMuseumImage(View v) {
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
         startActivity(intent);
-
     }
 
+    public void onClickCalculationButton(View v){
+
+        // Stores the amount of tickets selected for each type of ticket
+        int totalAdultTickets = Integer.parseInt( adultTix.getSelectedItem().toString() );
+        int totalSeniorTickets = Integer.parseInt( seniorTix.getSelectedItem().toString() );
+        int totalStudentTickets = Integer.parseInt( studentTix.getSelectedItem().toString() );
+
+        // Calculates the price of each set of tickets
+        double adultPriceTotal = totalAdultTickets * adultTicketPrice;
+        double seniorPriceTotal = totalSeniorTickets * seniorTicketPrice;
+        double studentPriceTotal = totalStudentTickets * studentTicketPrice;
+
+        // Calculates the final prices to be displayed
+        double finalTicketPrice = adultPriceTotal + seniorPriceTotal + studentPriceTotal;
+        double salesTaxPrice = finalTicketPrice * (salesTax/100);
+        double finalTicketTotal = finalTicketPrice + salesTaxPrice;
+
+        // Rounds the prices to the nearest cent
+        finalTicketPrice = Math.round( finalTicketPrice * 100 ) / 100;
+        salesTaxPrice = Math.round( salesTaxPrice * 100 ) / 100;
+        finalTicketTotal = Math.round( finalTicketTotal * 100 ) / 100;
+
+        //Sets the text
+        ticketPriceField.setText( "$" + Double.toString(finalTicketPrice) + "0" );
+        salesTaxField.setText( "$" + Double.toString(salesTaxPrice) + "0" );
+        ticketTotalField.setText( "$" + Double.toString(finalTicketTotal) + "0" );
+
+    }
 
 }
